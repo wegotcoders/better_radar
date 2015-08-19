be8512b77967ba27b7b677ecfd17d2cea3b1655b
Dan Garland <dan@dangarland.co.uk>
2015-08-19 16:20:30 +0100

    Parsing text out of tournaments

--- lib/better_radar/document.rb
+++ lib/better_radar/document.rb
@@ -6,9 +6,30 @@ class BetterRadar::Document < Nokogiri::XML::SAX::Document
 
   def start_element(name, attributes)
     case name
+    when 'Tournament', 'Match'
+      instance_variable_set("@last_#{name.downcase}", {
+        texts: []
+      })
+    when 'Text'
+      @inside_text = true
+    end
+  end
+
+  def characters(text)
+    if @inside_text && @last_tournament && !@last_match
+      content = text.strip.chomp
+      @last_tournament[:texts] << content unless content.empty?
+    end
+  end
+
+
+  def end_element(name)
+    case name
     when 'Tournament', 'Match', 'Odds'
       method_name = "handle_#{name.downcase}"
-      @handler.send(method_name) if @handler.respond_to? method_name
+      @handler.send(method_name, @last_tournament) if @handler.respond_to? method_name
+    when 'Text'
+      @inside_text = false
     end
   end
 end
--- spec/better_radar/parser_spec.rb
+++ spec/better_radar/parser_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 RSpec.describe BetterRadar::Parser do
 
   describe "parse method" do
+    let(:tournament_data) do
+      {
+        texts: %w(Championship Championship)
+      }
+    end
+
     let(:mock_handler) do
       mock_handler = mock
-      mock_handler.expects(:handle_tournament).once
+      mock_handler.expects(:handle_tournament).with(tournament_data).once
       mock_handler
     end
 
