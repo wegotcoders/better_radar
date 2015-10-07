7a382e43b7ffa69b0de014f90ec225c94f0419f8
kknevitt <knev123@hotmail.com>
2015-10-07 16:59:09 +0100

    adding some descriptions

--- lib/better_radar/element/bet.rb
+++ lib/better_radar/element/bet.rb
@@ -8,6 +8,21 @@ module BetterRadar::Element
       self.odds = []
     end
 
+    def type_description
+      case type
+      when '1'
+        'Handicap'
+      when '2'
+        'Correct Score'
+      when '10'
+        '3-Way'
+      when '20'
+        '2-Way'
+      when '60'
+        'Totals'
+      end
+    end
+
     def key_name
       'bet'
     end
